abea4d4a09f61ecd36f675eceeee323052e176cf
lic <121096190@qq..com>
2019-11-06 15:17:22 +0800

    去除请求时的弹窗

--- permission/src/main/java/com/yanzhenjie/permission/task/TaskExecutor.java
+++ permission/src/main/java/com/yanzhenjie/permission/task/TaskExecutor.java
@@ -29,25 +29,25 @@ public abstract class TaskExecutor<T> extends AsyncTask<Void, Void, T> {
 
     private static Executor sExecutor = Executors.newSingleThreadExecutor();
 
-    private Dialog mDialog;
+//    private Dialog mDialog;
 
     public TaskExecutor(Context context) {
-        this.mDialog = new WaitDialog(context);
-        this.mDialog.setCancelable(false);
+//        this.mDialog = new WaitDialog(context);
+//        this.mDialog.setCancelable(false);
     }
 
     @Override
     protected final void onPreExecute() {
-        if (!mDialog.isShowing()) {
-            mDialog.show();
-        }
+//        if (!mDialog.isShowing()) {
+//            mDialog.show();
+//        }
     }
 
     @Override
     protected final void onPostExecute(T t) {
-        if (mDialog.isShowing()) {
-            mDialog.dismiss();
-        }
+//        if (mDialog.isShowing()) {
+//            mDialog.dismiss();
+//        }
         onFinish(t);
     }
 
